eefea3c5644e482d145c545954f9a3e1bd8da1ee
dineshkarthick5025 <dineshkarthick282005@gmail.com>
2026-04-21 10:44:20 +0530

    fix: untrack node_modules and .env from git

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,10 @@ app.use((err, req, res, next) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+// For local development
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+}
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+export default app;
